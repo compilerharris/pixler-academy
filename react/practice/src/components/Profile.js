66e5616945787f7622438597bef41fd2c21dd585
Haris Shaikh <compilerharris@gmail.com>
2024-09-25 20:22:13 +0530

    Update syllabus and create day3 notes.

---
+++ react/practice/src/components/Profile.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className='profile'>
+          <p className='name'>Haris</p>
+          <img className='profile-img' src='https://imgd.aeplcdn.com/370x208/n/cw/ec/130591/fronx-exterior-right-front-three-quarter-109.jpeg?isig=0&q=80' alt='' />
+          <p className='about'>I am a developer</p>
+        </div>
+    )
+}
+
+export default Profile;
