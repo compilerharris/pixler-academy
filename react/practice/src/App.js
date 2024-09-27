c1bb5e9b7fb9e0d569dd79bb62aca9ea44bf26ba
Haris Shaikh <compilerharris@gmail.com>
2024-09-27 21:16:26 +0530

    Create day5 notes and update syllabus.

--- react/practice/src/App.js
+++ react/practice/src/App.js
@@ -2,10 +2,31 @@ import './App.css';
 import Profile from './components/Profile';
 
 function App() {
+  const profiles = [
+    {
+      name:"Samit",
+      img:"https://t3.ftcdn.net/jpg/02/16/60/42/360_F_216604243_4yvQhIudgknAWu04Ob7aSOu6BdIfhBNb.jpg",
+      about:"I am a web designer",
+    },
+    {
+      name:"Huzaif",
+      img:"https://www.motoroids.com/wp-content/uploads/2015/04/Fast-and-Furious-Toyota-Supra-10.jpg",
+    },
+    {
+      name:"Rehan",
+      img:"https://img.lovepik.com/photo/48007/1949.jpg_wh860.jpg",
+      about:"I am a developer",
+    }
+  ];
+
   return (
     <div>
       <h1>Profiles</h1>
-      <Profile name={"samit"} />
+      {
+        profiles.map((profile, key) => {
+          return <Profile key={key} name={profile.name} imgUrl={profile.img} about={profile.about} />
+        })
+      }
     </div>
   );
 }
--- react/practice/src/components/Profile.js
+++ react/practice/src/components/Profile.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Profile = ({name}) => {
+const Profile = ({name, imgUrl, about}) => {
     return (
         <div className='profile'>
           <p className='name'>{name}</p>
-          <img className='profile-img' src='https://imgd.aeplcdn.com/370x208/n/cw/ec/130591/fronx-exterior-right-front-three-quarter-109.jpeg?isig=0&q=80' alt='' />
-          <p className='about'>I am a developer</p>
+          <img className='profile-img' src={imgUrl} alt='' />
+          <p className='about'>{about}</p>
         </div>
     )
 }
