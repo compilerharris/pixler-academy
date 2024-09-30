d63cf490a8b393330acf4b6f19c51a9f6545ee0c
Haris Shaikh <compilerharris@gmail.com>
2024-09-30 21:57:19 +0530

    Create day6 notes and update syllabus.

--- react/practice/src/App.js
+++ react/practice/src/App.js
@@ -7,26 +7,42 @@ function App() {
       name:"Samit",
       img:"https://t3.ftcdn.net/jpg/02/16/60/42/360_F_216604243_4yvQhIudgknAWu04Ob7aSOu6BdIfhBNb.jpg",
       about:"I am a web designer",
+      profileUrl:"https://github.com/samit46"
     },
     {
       name:"Huzaif",
       img:"https://www.motoroids.com/wp-content/uploads/2015/04/Fast-and-Furious-Toyota-Supra-10.jpg",
+      about:"I am a student",
+      profileUrl:"https://github.com/itachi644"
     },
     {
       name:"Rehan",
       img:"https://img.lovepik.com/photo/48007/1949.jpg_wh860.jpg",
       about:"I am a developer",
+      profileUrl:"https://github.com/rehandossa"
+    },
+    {
+      name:"Muizz",
+      img:"https://img.lovepik.com/photo/48007/1949.jpg_wh860.jpg",
+      about:"I am a Student",
+      profileUrl:"https://github.com/skmuizz"
     }
   ];
 
   return (
-    <div>
-      <h1>Profiles</h1>
-      {
-        profiles.map((profile, key) => {
-          return <Profile key={key} name={profile.name} imgUrl={profile.img} about={profile.about} />
-        })
-      }
+    <div className="container mt-5">
+      <h1>Github Profiles</h1>
+      <div className="row">
+        {
+          profiles.map((profile, key) => {
+            return (
+              <div className="col-md-4" key={key} >
+                <Profile profile={profile} />
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   );
 }
--- react/practice/src/components/Profile.js
+++ react/practice/src/components/Profile.js
@@ -1,12 +1,19 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
-const Profile = ({name, imgUrl, about}) => {
+const Profile = ({profile}) => {
     return (
-        <div className='profile'>
-          <p className='name'>{name}</p>
-          <img className='profile-img' src={imgUrl} alt='' />
-          <p className='about'>{about}</p>
-        </div>
+      <Card className="mb-2" style={{ width: '18rem' }}>
+        <Card.Img variant="top" src={profile.img} style={{ width: '18rem',height: '18rem' }} />
+        <Card.Body>
+          <Card.Title>{profile.name}</Card.Title>
+          <Card.Text>{profile.about}</Card.Text>
+          <Card.Link href={profile.profileUrl} target="_blank">
+            <Button variant="primary">Visit {profile.name}'s Profile</Button>
+          </Card.Link>
+        </Card.Body>
+      </Card>
     )
 }
 
